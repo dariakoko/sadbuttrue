4117be7bec84ee2c2102ee42da2883ad7bd2fd62
Daria <nargle30@gmail.com>
2019-11-16 10:31:07 +0200

    New project created

---
+++ sadbuttrue/sadbuttrue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sadbuttrue
+//
+//  Created by Daria Kozlova on 16.11.2019.
+//  Copyright © 2019 Daria Kozlova. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
